af01597aa613e1f829e66fc55e4a43a43dbc2a06
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-05-16 14:45:29 -0500

    defined the businessHRs variable as an array with hr intervals between 8am and 5pm

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,6 +1,19 @@
 // Global variable set to get the current date at time.
 var day = moment();
-var businessHRs = moment.duration(9, "hours");
+var businessHRs = [
+	"8AM",
+	"9AM",
+	"10AM",
+	"11AM",
+	"12PM",
+	"1PM",
+	"2PM",
+	"3PM",
+	"4PM",
+	"5PM",
+];
+
+console.log(businessHRs);
 
 // Used JQuery to target the <p> tag in the jumbortron header to include a dynamic date.
 $("#currentDay").text(day.format("dddd MMMM Do YYYY"));
