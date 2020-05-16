223846d1e43abef8c1db940cf518d3d41145621a
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-05-16 17:56:26 -0500

    modified save button for-loop and added pseudocode

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -26,10 +26,17 @@ for (var i = 0; i < businessHRs.length; i++) {
 }
 // Created a for-loop to bind the comment text areas with the save button for each business hour.
 for (var i = 0; i < 10; i++) {
+	// In this in line, I am targetting the button ID to the following anonymous function.
 	$("#button" + i).click(function () {
+		//This variable targets the assigned value for each button.
 		var buttonIndex = $(this).val();
+		//This variable is used to target entered comment text for each timeblock based on the related save button.
 		var commentText = $("#comment" + buttonIndex).val();
+		/*Here were are appending all entered comment text to the global toDoItems array by passing throughout the 
+		the index per save button bound to each related comment text ID in the index.html file. */
 		toDoItems[buttonIndex] = commentText;
+		/*Using local storage to store any comment entries appended into the toDoItems array. This will allow each 
+		comment entry per timeblock to save after a refresh once the save button per timeblock is clicked.*/
 		localStorage.setItem("toDoItems", JSON.stringify(toDoItems));
 	});
 }
