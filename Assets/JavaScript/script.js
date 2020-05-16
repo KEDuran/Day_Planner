f900e9747012041ddf5dd5e2c0ca19cfcf4c4b47
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-05-16 17:40:53 -0500

    added for-loop, with function, to save all text areas entries

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -13,19 +13,23 @@ var businessHRs = [
 	"4PM",
 	"5PM",
 ];
+// Global variable to store text area comments.
+var toDoItems = ["", "", "", "", "", "", "", "", "", ""];
 
 // Used JQuery to target the <p> tag in the jumbortron header to include a dynamic date.
 $("#currentDay").text(day.format("dddd, MMMM Do YYYY"));
-
-// This function will let users save entry items in local storage.
-
-function saveToDos(buttonIndex) {
-	var items = $("#comment" + buttonIndex).text();
-	console.log(items);
-}
 
 // Created a for-loop to iterate through the businessHRs array variable.
 for (var i = 0; i < businessHRs.length; i++) {
 	// This targets the timeInterval ID and concatenates var i to apply a business hour to each timeblock.
 	$("#timeInterval" + i).text(businessHRs[i]);
+}
+// Created a for-loop to bind the comment text areas with the save button for each business hour.
+for (var i = 0; i < 10; i++) {
+	$("#button" + i).click(function () {
+		var buttonIndex = $(this).val();
+		var commentText = $("#comment" + buttonIndex).val();
+		toDoItems[buttonIndex] = commentText;
+		localStorage.setItem("toDoItems", JSON.stringify(toDoItems));
+	});
 }
