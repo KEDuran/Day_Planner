5809c78519880952d179bbfc7b28da3ad52004ae
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-05-16 22:14:14 -0500

    added some last minute pseudocode modifications

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,54 +1,62 @@
-// Global variable set to get the current date at time.
+// Global variable set to retreive the current date the day planner app is opened.
 var m = moment();
-// Global variable that has total number of business hours per day.
+/* Global variable with an empty array that will allow the business hours populated in the for-loop below to 
+be pushed into this businesHrs array variable.*/
 var businessHRs = [];
-// Global variable to store text area comments.
+// Global variable with an empty array to store text area comments saved in local storage for each timeblock.
 var toDoItems = ["", "", "", "", "", "", "", "", "", ""];
 
-// Invoking the retain() function here to make sure all saved toDoItems populate upon a page refresh.
+// Invoking the retain() function here to make sure all saved toDoItems re-populate upon  page refresh.
 retain();
 
-// Used JQuery to target the <p> tag in the jumbortron header to include a dynamic date.
+// Used JQuery to target <p> tag in the jumbortron header to include a dynamic date.
 $("#currentDay").text(m.format("dddd, MMMM Do YYYY"));
 
-// Created a for-loop to add moment objects to businessHRs array and bind the comment text areas with the save button for each business hour.
+/*Created a for-loop to add moment objects to the businessHRs array and bind the comment text areas with the 
+save button for each business hour.*/
 for (var i = 0; i < 10; i++) {
-	// Adds a new moment object to the businessHRs array to include a 8am - 5pm business day.
+	// Adds a new moment object to the businessHRs array to reflect an 8am - 5pm business day.
 	businessHRs.push(moment({ hour: i + 8 }));
-	// This targets the timeInterval ID to apply a business hour to each timeblock.
+	// This targets the timeInterval ID to apply each businessHRs array index to each matching timeblock.
 	$("#timeInterval" + i).text(businessHRs[i].format("hA"));
 
+	/* This if statement adds the coloration for past, present, and future business hours based on the time 
+	assigned to each timeblock.*/
+	// Applies the past class CSS selector where applicable by using .isAfter() moment.js function.
 	if (m.isAfter(businessHRs[i])) {
 		$("#comment" + i).addClass("past");
 	} else if (
+		// Applies the present class CSS selector where applicable by using .isBetween() moment.js function.
 		m.isBetween(businessHRs[i], moment({ hour: i + 9 }), undefined, "[]")
 	) {
 		$("#comment" + i).addClass("present");
 	} else {
+		// Applies the future class CSS selector where applicable if other parameters in this statement are not met.
 		$("#comment" + i).addClass("future");
 	}
 
-	// In this in line, I am targetting the button ID to the following anonymous function.
+	// In this line, the button ID are targetted using the following anonymous function.
 	$("#button" + i).click(function () {
 		//This variable targets the assigned value for each button.
 		var buttonIndex = $(this).val();
-		//This variable is used to target entered comment text for each timeblock based on the related save button.
+		//This variable is used to target each entered comment text per timeblock based on the related save button.
 		var commentText = $("#comment" + buttonIndex).val();
-		/*Here were are appending all entered comment text to the global toDoItems array by passing throughout the 
-		the index per save button bound to each related comment text ID in the index.html file. */
+		/*Each entered comment text is appended to the global toDoItems array by passing throughout the buttonIndex, 
+		which is bound via the previously declared commentText variable.*/
 		toDoItems[buttonIndex] = commentText;
-		/*Using local storage to store any comment entries appended into the toDoItems array. This will allow each 
-		comment entry per timeblock to save after a refresh once the save button per timeblock is clicked.*/
+		/*This uses local storage to store any comment entries appended into the toDoItems array. This will allow each 
+		comment entry per timeblock to save once the save button per timeblock is clicked.*/
 		localStorage.setItem("toDoItems", JSON.stringify(toDoItems));
 	});
 
-	/* This JQuery selector statement targets each timeblock's text area and will populate the to do item in the toDoItems 
-	array with a matching index value.*/
+	/* This statement targets each timeblock's text area and will populate the saved to-do item in the toDoItems array 
+	that has a matching index value.*/
 	$("#comment" + i).val(toDoItems[i]);
 }
 
 /* The retain() function is parsing out the toDoItems items that are saved in local storage and reassigning them back to the 
-associated timeblock, which ensures that the saved toDoItems populate after the a page refresh. */
+appropriate timeblock, which ensures that the saved toDoItems populate after the a page refresh. This function is called at the 
+beginning of the this code.*/
 function retain() {
 	var storedtoDoItems = JSON.parse(localStorage.getItem("toDoItems"));
 	if (storedtoDoItems !== null) {
